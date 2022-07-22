04679f365b2211ab2485d1fe65a9fea80ab2521d
Adam Chambers <adammchambers@gmail.com>
2022-07-22 15:16:29 -0400

    Install OpenZepplin and ERC721 scaffolding

---
+++ contracts/Zeitgeist.sol
@@ -0,0 +1,33 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Zeitgeist is ERC721URIStorage, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("Zeitgeist", "ZEIT") {}
+
+  function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _mint(recipient, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    return newItemId;
+  }
+
+  function withdraw() public payable onlyOwner {
+    uint balance = address(this).balance;
+    require(balance > 0, "No ether left to withdraw");
+
+    (bool success, ) = (msg.sender).call{value: balance}("");
+    require(success, "Transfer failed.");
+  }
+}
