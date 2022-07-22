6e997d2ac8e728bcce78c11fa0ca13a32669347e
Adam Chambers <adammchambers@gmail.com>
2022-07-22 15:14:30 -0400

    Reduce rounded corners on button

--- components/button.tsx
+++ components/button.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Button = ({ children, className, href, target, rel }: Props) => {
   const classNames =
-    'inline-flex items-center justify-center py-3 pl-5 pr-6 mx-auto transition duration-300 bg-teal-500 rounded-lg hover:fill-zinc-800 fill-white group text-zinc-900 hover:bg-white ' +
+    'inline-flex items-center justify-center py-3 pl-5 pr-6 mx-auto transition duration-300 bg-teal-500 rounded-md hover:fill-zinc-800 fill-white group text-zinc-900 hover:bg-white ' +
     className
 
   const link = (
